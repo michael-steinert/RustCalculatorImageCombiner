0cae20c2651d52b42afdd23c30b5c191e6f49534
Michael Steinert <Michael.Steinert.94@gmail.com>
2023-02-07 22:23:08 +0100

    Added Lifetime

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 struct User<'b> {
   name: &'b str
 }
@@ -13,12 +15,15 @@ fn main() {
   let str1: String = String::from("Michael");
   {
     let str2: String = String::from("Bruno"); 
-    let longest_string: &str = get_longest(str1.as_str(), str2.as_str());
-    println!("Longest String is {}", longest_string);
+    let longest_name: &str = get_longest_name(str1.as_str(), str2.as_str());
+    println!("Longest String is {}", longest_name);
+
+    let longest_name_with_greeting: &str = get_longest_name_with_greeting(str1.as_str(), str2.as_str(), "Hey");
+    println!("Longest Name with Greeting is {}", longest_name_with_greeting);
   }
   // Does not work because the Return Type has the same Lifetime as the Parameter with the smallest Lifetime
   // str2 in the inner Scope has shorter Lifetime than str1 in the outer therefore the Return Type is here not valid
-  // println!("Longest String is {}", longest_string);
+  // println!("Longest String is {}", longest_name);
   {
     /* Struct is only valid if the used Variable `str1` is in its Scope */
     let user: User = User {
@@ -37,10 +42,27 @@ fn main() {
 &'a mut i32 - a mutable Reference with an explicit Lifetime
 */
 /* The Return Type will have the smallest Lifetime of the given Parameter */
-fn get_longest<'a>(a: &'a str, b: &'a str) -> &'a str {
+fn get_longest_name<'a>(a: &'a str, b: &'a str) -> &'a str {
   if a.len() > b.len() {
     a
   } else {
     b
   }
 }
+
+fn get_longest_name_with_greeting<'c, T>(
+  a: &'c str,
+  b: &'c str,
+  greeting: T
+) -> &'c str
+where
+  T:Display
+{
+  if a.len() > b.len() {
+    println!("{} {}", greeting, a);
+    a
+  } else {
+     println!("{} {}", greeting, b);
+    b
+  }
+}  
